19b1be66586dd16608502aac56e68d0735b3b570
Rajesh Puchakayala <rajesh.puchakayala@wavicledata.com>
2025-06-01 18:02:01 +0530

    Add ECS task module

--- modules/ecs-task/outputs.tf
+++ modules/ecs-task/outputs.tf
@@ -1,7 +1,9 @@
-output "ecs_service_arn" {
-  value = aws_ecs_service.grafana.arn
-}
-
-output "ecs_service_name" {
-  value = aws_ecs_service.grafana.name
-}
+1. output "ecs_service_id" {
+2.   description = "The ECS service ID (ARN)"
+3.   value       = aws_ecs_service.grafana.id
+4. }
+5. 
+6. output "ecs_service_name" {
+7.   description = "The ECS service name"
+8.   value       = aws_ecs_service.grafana.name
+9. }
