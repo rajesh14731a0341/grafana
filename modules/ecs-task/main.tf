90452526b1c438c9cdbf50ef3f6f29507901d170
Rajesh Puchakayala <rajesh.puchakayala@wavicledata.com>
2025-06-01 16:08:28 +0530

    Add ECS task module

--- modules/ecs-task/main.tf
+++ modules/ecs-task/main.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "grafana" {
-  family                   = "rajeshso-grafana-task"
-  requires_compatibilities = ["FARGATE"]
+  family                   = "rajesh-grafana-task"
   network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
   memory                   = "1024"
   execution_role_arn       = var.execution_role_arn
@@ -10,80 +10,92 @@ resource "aws_ecs_task_definition" "grafana" {
   container_definitions = jsonencode([
     {
       name      = "grafana"
-      image     = "grafana/grafana-enterprise:11.6.1"
-      portMappings = [{ containerPort = 3000, hostPort = 3000 }]
-      environment = [
-        { name = "GF_SECURITY_ADMIN_USER", value = var.grafana_user },
-        { name = "GF_SECURITY_ADMIN_PASSWORD", value = var.grafana_password },
-        { name = "GF_RENDERING_SERVER_URL", value = "http://localhost:8081/render" },
-        { name = "GF_RENDERING_CALLBACK_URL", value = "http://localhost:3000/" },
-        { name = "GF_PLUGIN_ALLOW_LOCAL_MODE", value = "true" }
-      ]
-      mountPoints = [{
-        containerPath = "/var/lib/grafana",
-        sourceVolume  = "grafana-data"
-      }]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "/ecs/rajeshso-grafana"
-          awslogs-region        = var.region
-          awslogs-stream-prefix = "ecs"
+      image     = var.grafana_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 3000
+          protocol      = "tcp"
         }
-      }
+      ]
+      environment = [
+        {
+          name  = "GF_SECURITY_ADMIN_USER"
+          value = var.grafana_user
+        },
+        {
+          name  = "GF_SECURITY_ADMIN_PASSWORD"
+          value = var.grafana_password
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "grafana_data"
+          containerPath = "/var/lib/grafana"
+          readOnly      = false
+        }
+      ]
     },
     {
       name      = "renderer"
-      image     = "grafana/grafana-image-renderer:3.12.5"
-      portMappings = [{ containerPort = 8081, hostPort = 8081 }]
-      environment = [
-        { name = "RENDERER_USER", value = var.renderer_user },
-        { name = "RENDERER_PASSWORD", value = var.renderer_password }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "/ecs/rajeshso-grafana"
-          awslogs-region        = var.region
-          awslogs-stream-prefix = "ecs"
+      image     = var.renderer_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8081
+          protocol      = "tcp"
         }
-      }
+      ]
+      environment = [
+        {
+          name  = "RENDERER_USER"
+          value = var.renderer_user
+        },
+        {
+          name  = "RENDERER_PASSWORD"
+          value = var.renderer_password
+        }
+      ]
     },
     {
       name      = "redis"
-      image     = "redis:7"
-      portMappings = [{ containerPort = 6379, hostPort = 6379 }]
-      environment = [
-        { name = "REDIS_USER", value = var.redis_user },
-        { name = "REDIS_PASSWORD", value = var.redis_password }
-      ]
-      logConfiguration = {
-        logDriver = "awslogs"
-        options = {
-          awslogs-group         = "/ecs/rajeshso-grafana"
-          awslogs-region        = var.region
-          awslogs-stream-prefix = "ecs"
+      image     = var.redis_image
+      essential = true
+      portMappings = [
+        {
+          containerPort = 6379
+          protocol      = "tcp"
         }
-      }
+      ]
+      environment = [
+        {
+          name  = "REDIS_USER"
+          value = var.redis_user
+        },
+        {
+          name  = "REDIS_PASSWORD"
+          value = var.redis_password
+        }
+      ]
     }
   ])
 
   volume {
-    name = "grafana-data"
+    name = "grafana_data"
     efs_volume_configuration {
-      file_system_id          = var.filesystem_id
-      transit_encryption      = "ENABLED"
+      file_system_id          = var.efs_file_system_id
       authorization_config {
-        access_point_id = var.access_point_id
+        access_point_id = var.efs_access_point_id
         iam             = "ENABLED"
       }
+      transit_encryption = "ENABLED"
     }
   }
 }
 
 resource "aws_ecs_service" "grafana" {
-  name            = "rajeshso-grafana-service"
-  cluster         = var.cluster_arn
+  name            = "rajesh-grafana-service"
+  cluster         = var.ecs_cluster_id
   task_definition = aws_ecs_task_definition.grafana.arn
   desired_count   = 1
   launch_type     = "FARGATE"
@@ -91,7 +103,7 @@ resource "aws_ecs_service" "grafana" {
   network_configuration {
     subnets         = var.subnet_ids
     security_groups = [var.security_group_id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
   depends_on = [aws_ecs_task_definition.grafana]
--- modules/ecs-task/outputs.tf
+++ modules/ecs-task/outputs.tf
@@ -1,3 +1,9 @@
 output "service_arn" {
-  value = aws_ecs_service.grafana.arn
+  description = "ARN of the ECS Grafana service"
+  value       = aws_ecs_service.grafana.arn
+}
+
+output "task_definition_arn" {
+  description = "ARN of the ECS Grafana task definition"
+  value       = aws_ecs_task_definition.grafana.arn
 }
