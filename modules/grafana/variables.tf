12d966ce65313817dec40f7327191daced600d70
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-05 16:17:40 +0530

    fix: terraform fmt formatting

--- modules/grafana/variables.tf
+++ modules/grafana/variables.tf
@@ -1,24 +1,117 @@
-variable "ecs_cluster_id" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "security_group_id" { type = string }
-variable "execution_role_arn" { type = string }
-variable "task_role_arn" { type = string }
+variable "ecs_cluster_id" {
+  description = "ECS Cluster ARN"
+  type        = string
+}
 
-variable "postgres_secret_arn" { type = string }
-variable "postgres_host" { type = string }
-variable "postgres_port" { type = number }
-variable "postgres_db" { type = string }
+variable "subnet_ids" {
+  description = "List of subnet IDs"
+  type        = list(string)
+}
 
-variable "desired_count_grafana" { type = number }
-variable "min_capacity_grafana" { type = number }
-variable "max_capacity_grafana" { type = number }
+variable "security_group_id" {
+  description = "Security Group ID"
+  type        = string
+}
 
-variable "desired_count_renderer" { type = number }
-variable "min_capacity_renderer" { type = number }
-variable "max_capacity_renderer" { type = number }
+variable "execution_role_arn" {
+  description = "IAM Role ARN for ECS task execution"
+  type        = string
+}
 
-variable "desired_count_redis" { type = number }
-variable "min_capacity_redis" { type = number }
-variable "max_capacity_redis" { type = number }
+variable "task_role_arn" {
+  description = "IAM Role ARN for ECS task"
+  type        = string
+}
 
-variable "cpu_target" { type = number }
+variable "postgres_secret_arn" {
+  description = "Secrets Manager ARN for PostgreSQL credentials"
+  type        = string
+}
+
+variable "postgres_host" {
+  description = "PostgreSQL host endpoint"
+  type        = string
+}
+
+variable "postgres_port" {
+  description = "PostgreSQL port"
+  type        = number
+  default     = 5432
+}
+
+variable "postgres_db" {
+  description = "PostgreSQL database name"
+  type        = string
+}
+
+variable "desired_count_grafana" {
+  description = "Desired number of Grafana tasks"
+  type        = number
+  default     = 1
+}
+
+variable "min_capacity_grafana" {
+  description = "Minimum number of Grafana tasks for autoscaling"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity_grafana" {
+  description = "Maximum number of Grafana tasks for autoscaling"
+  type        = number
+  default     = 5
+}
+
+variable "cpu_target_grafana" {
+  description = "CPU utilization target for Grafana autoscaling"
+  type        = number
+  default     = 70
+}
+
+variable "desired_count_renderer" {
+  description = "Desired number of Renderer tasks"
+  type        = number
+  default     = 1
+}
+
+variable "min_capacity_renderer" {
+  description = "Minimum number of Renderer tasks for autoscaling"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity_renderer" {
+  description = "Maximum number of Renderer tasks for autoscaling"
+  type        = number
+  default     = 5
+}
+
+variable "cpu_target_renderer" {
+  description = "CPU utilization target for Renderer autoscaling"
+  type        = number
+  default     = 70
+}
+
+variable "desired_count_redis" {
+  description = "Desired number of Redis tasks"
+  type        = number
+  default     = 1
+}
+
+variable "min_capacity_redis" {
+  description = "Minimum number of Redis tasks for autoscaling"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity_redis" {
+  description = "Maximum number of Redis tasks for autoscaling"
+  type        = number
+  default     = 5
+}
+
+variable "cpu_target_redis" {
+  description = "CPU utilization target for Redis autoscaling"
+  type        = number
+  default     = 70
+}
