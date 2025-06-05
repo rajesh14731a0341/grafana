a50465fdd6512f5e8cdc5af66920ec9dfab86452
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-05 16:16:08 +0530

    fix: terraform fmt formatting

--- modules/grafana/outputs.tf
+++ modules/grafana/outputs.tf
@@ -1,6 +1,19 @@
 output "grafana_service_arn" {
-  value = aws_ecs_service.grafana.arn
+  description = "ARN of the Grafana ECS service"
+  value       = aws_ecs_service.grafana.arn
 }
 
 output "renderer_service_arn" {
-  value = aws_ecs
+  description = "ARN of the Renderer ECS service"
+  value       = aws_ecs_service.renderer.arn
+}
+
+output "redis_service_arn" {
+  description = "ARN of the Redis ECS service"
+  value       = aws_ecs_service.redis.arn
+}
+
+output "ecs_cluster_arn" {
+  description = "ARN of the ECS Cluster"
+  value       = aws_ecs_cluster.main.arn
+}
