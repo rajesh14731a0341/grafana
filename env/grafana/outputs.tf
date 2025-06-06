a7c700d021aa23b33fa35568bfccc350b5532d9b
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-06 17:18:37 +0530

    Initial commit on grafana_cloudmap branch

--- env/grafana/outputs.tf
+++ env/grafana/outputs.tf
@@ -1,11 +1,14 @@
 output "grafana_service_name" {
-  value = module.grafana.grafana_service_name
+  description = "The name of the Grafana ECS service"
+  value       = module.grafana.grafana_service_name
 }
 
-output "renderer_service_name" {
-  value = module.grafana.renderer_service_name
+output "grafana_task_definition_arn" {
+  description = "The ARN of the Grafana ECS task definition"
+  value       = module.grafana.grafana_task_definition_arn
 }
 
-output "redis_service_name" {
-  value = module.grafana.redis_service_name
+output "grafana_log_group_name" {
+  description = "The CloudWatch log group name for Grafana"
+  value       = module.grafana.grafana_log_group_name
 }
