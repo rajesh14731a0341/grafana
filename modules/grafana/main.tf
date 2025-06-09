ca8c4aec49c7bcfeb81eee9a71e44ecc8042b2ce
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-09 13:47:11 +0530

    Add grafana module with cloud map support

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -275,10 +275,25 @@ resource "aws_appautoscaling_policy" "grafana_cpu" {
 resource "aws_appautoscaling_target" "renderer" {
   max_capacity       = var.renderer_autoscaling_max
   min_capacity       = var.renderer_autoscaling_min
-  resource_id        = "service/${var.ecs_cluster_id}/${aws_ecs_service.renderer.name}"
+  resource_id        = "service/${var.ecs_cluster_name}/${aws_ecs_service.renderer.name}"  # Use cluster **name** here, not full ARN
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
 resource "aws_appautoscaling_policy" "renderer_cpu" {
-  name               = "renderer-cpu"
+  name               = "renderer-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.renderer.resource_id
+  scalable_dimension = aws_appautoscaling_target.renderer.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.renderer.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    target_value       = var.renderer_autoscaling_cpu_target
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    scale_in_cooldown  = 60
+    scale_out_cooldown = 60
+  }
+}
+
