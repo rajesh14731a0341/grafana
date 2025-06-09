3e4f697270cb6eaa7dd06f587e80ae7facfe0865
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-09 18:26:00 +0530

    Add grafana module with cloud map support

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -107,7 +107,7 @@ resource "aws_ecs_task_definition" "renderer" {
       protocol      = "tcp"
     }]
     healthCheck = {
-      command     = ["CMD-SHELL", "curl -f http://localhost:8081/render/version || exit 1"]
+      command     = ["CMD-SHELL", "wget -qO- http://localhost:8081/render/version || exit 1"]
       interval    = 30
       timeout     = 5
       retries     = 3
