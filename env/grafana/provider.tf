eb622cfbb11b471107e8151f4f7614a5fc826499
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-09 14:31:43 +0530

    Add grafana module with cloud map support

--- env/grafana/provider.tf
+++ env/grafana/provider.tf
@@ -1,3 +1,13 @@
+# env/grafana/provider.tf
 provider "aws" {
   region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Specify a recent version here
+    }
+  }
 }
