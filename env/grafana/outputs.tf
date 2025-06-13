739fee0c51006f13530c6797946ced9915eca189
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-13 13:29:10 +0530

    Added ALB setup and ECS services for Grafana, Renderer, and Redis

--- env/grafana/outputs.tf
+++ env/grafana/outputs.tf
@@ -1,11 +1,11 @@
 output "grafana_alb_dns" {
-  value = module.grafana.grafana_alb_dns
-}
-
-output "renderer_target_group" {
-  value = module.grafana.renderer_target_group_arn
+  value = module.grafana.grafana_alb_dns_name
 }
 
 output "redis_nlb_dns" {
-  value = module.grafana.redis_nlb_dns
+  value = module.grafana.internal_nlb_dns_name
+}
+
+output "renderer_service_name" {
+  value = module.grafana.renderer_service_name
 }
