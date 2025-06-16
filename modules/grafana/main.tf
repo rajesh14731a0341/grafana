bc64e06e7e21e56f9550566279f4133619d9fedd
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-16 12:09:40 +0530

    Added ALB setup and ECS services for Grafana, Renderer, and Redis

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -53,7 +53,7 @@ resource "aws_lb_target_group" "renderer_tg" {
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
-    unhealthy_threshold = 10
+    unhealthy_threshold = 2
   }
 }
 
