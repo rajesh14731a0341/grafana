592a1d83cc483cca4a6c454cba2b08cf860a213a
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-16 16:05:56 +0530

    Added ALB setup and ECS services for Grafana, Renderer, and Redis

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -203,26 +203,27 @@ resource "aws_ecs_task_definition" "renderer" {
   task_role_arn            = var.task_role_arn
 
   container_definitions = jsonencode([{
-  name  = "renderer"
-  image = "grafana/grafana-image-renderer:3.11.0"
-  portMappings = [{
-    containerPort = 8081
-  }]
-  environment = [
-    { name = "RENDERER_AUTH_TOKEN_ENABLED", value = "false" },
-    { name = "RENDERER_AUTH_TOKEN_REQUIRED", value = "false" },
-    { name = "RENDERER_AUTH_TOKEN", value = "" }
-  ]
-  logConfiguration = {
-    logDriver = "awslogs",
-    options = {
-      awslogs-group         = aws_cloudwatch_log_group.renderer_log_group.name,
-      awslogs-region        = "us-east-1",
-      awslogs-stream-prefix = "renderer"
+    name  = "renderer"
+    image = "grafana/grafana-image-renderer:3.11.1"
+    portMappings = [{
+      containerPort = 8081
+    }]
+    environment = [
+      { name = "RENDERER_AUTH_TOKEN_ENABLED", value = "false" },
+      { name = "RENDERER_AUTH_TOKEN_REQUIRED", value = "false" },
+      { name = "RENDERER_AUTH_TOKEN", value = "" }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs",
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.renderer_log_group.name,
+        awslogs-region        = "us-east-1",
+        awslogs-stream-prefix = "renderer"
+      }
     }
-  }
-}])
+  }])
 }
+
 
 
 
