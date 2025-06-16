4c2fc0a17a641f36db854b00847bf58638c0a188
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-16 19:08:44 +0530

    Added ALB setup and ECS services for Grafana, Renderer, and Redis

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -152,7 +152,7 @@ resource "aws_ecs_task_definition" "grafana" {
       { name = "GF_RENDERING_SERVER_URL", value = "http://${aws_lb.public_alb.dns_name}/render" },
       { name = "GF_RENDERING_CALLBACK_URL", value = "http://${aws_lb.public_alb.dns_name}/render" },
       { name = "GF_PLUGIN_RENDERING_SERVER_URL", value = "http://${aws_lb.public_alb.dns_name}/render" },
-      { name = "GF_PLUGIN_RENDERING_CALLBACK_URL", value = "http://${aws_lb.public_alb.dns_name}/render" },
+      { name = "GF_PLUGIN_RENDERING_CALLBACK_URL", value = "http://${aws_lb.public_alb.dns_name}/" },
       { name = "GF_RENDERING_SERVER_ENABLE_AUTH", value = "false" },
       { name = "GF_PLUGIN_ALLOW_LOCAL_MODE", value = "true" },
       { name = "GF_LOG_FILTERS", value = "rendering:debug" }
