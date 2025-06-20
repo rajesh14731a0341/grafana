e254817af496f016f56cd99729e9cdec6574f156
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-20 12:43:34 +0530

    Initial commit for grafana ECS with Cloud Map and ALB

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -13,14 +13,14 @@ data "aws_lb_listener" "http" {
 # ---------------------------------------------
 # Target Group & Listener Rule for Grafana
 # ---------------------------------------------
-resource "aws_lb_target_group" "grafana" {
-  name        = "tg-grafana"
+resource "aws_lb_target_group" "grafana_cloudmap" {
+  name        = "tg-grafana-cloudmap"
   port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
   health_check {
-    path                = "/"
+    path                = "/grafana/login"
     protocol            = "HTTP"
     interval            = 30
     timeout             = 5
@@ -29,16 +29,16 @@ resource "aws_lb_target_group" "grafana" {
   }
 }
 
-resource "aws_lb_listener_rule" "grafana" {
+resource "aws_lb_listener_rule" "grafana_cloudmap" {
   listener_arn = data.aws_lb_listener.http.arn
   priority     = 100
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana.arn
+    target_group_arn = aws_lb_target_group.grafana_cloudmap.arn
   }
   condition {
     path_pattern {
-      values = ["/grafana*"]
+      values = ["/grafana", "/grafana/*"]
     }
   }
 }
@@ -46,8 +46,8 @@ resource "aws_lb_listener_rule" "grafana" {
 # ---------------------------------------------
 # Task Definitions
 # ---------------------------------------------
-resource "aws_ecs_task_definition" "grafana" {
-  family                   = "grafana-task"
+resource "aws_ecs_task_definition" "grafana_cloudmap" {
+  family                   = "grafana-cloudmap-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
@@ -57,17 +57,17 @@ resource "aws_ecs_task_definition" "grafana" {
 
   container_definitions = jsonencode([
     {
-      name      = "grafana"
+      name      = "grafana-cloudmap"
       image     = "grafana/grafana-enterprise:11.6.1"
       portMappings = [{ containerPort = 3000 }]
       environment = [
-        { name = "REDIS_PATH", value = "redis:6379" },
+        { name = "REDIS_PATH", value = "redis-cloudmap:6379" },
         { name = "REDIS_DB", value = "1" },
         { name = "REDIS_CACHETIME", value = "12000" },
         { name = "CACHING", value = "Y" },
         { name = "GF_PLUGIN_ALLOW_LOCAL_MODE", value = "true" },
-        { name = "GF_RENDERING_SERVER_URL", value = "http://renderer:8081/render" },
-        { name = "GF_RENDERING_CALLBACK_URL", value = "http://grafana:3000/" },
+        { name = "GF_RENDERING_SERVER_URL", value = "http://renderer-cloudmap:8081/render" },
+        { name = "GF_RENDERING_CALLBACK_URL", value = "http://grafana-cloudmap:3000/" },
         { name = "GF_LOG_FILTERS", value = "rendering: debug" },
         { name = "GF_DATABASE_HOST", value = var.db_endpoint },
         { name = "GF_DATABASE_NAME", value = var.db_name },
@@ -77,9 +77,9 @@ resource "aws_ecs_task_definition" "grafana" {
       logConfiguration = {
         logDriver = "awslogs",
         options = {
-          awslogs-group         = "/ecs/grafana"
+          awslogs-group         = "/ecs/grafana-cloudmap"
           awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = "grafana"
+          awslogs-stream-prefix = "grafana-cloudmap"
           awslogs-create-group  = "true"
         }
       }
@@ -87,8 +87,8 @@ resource "aws_ecs_task_definition" "grafana" {
   ])
 }
 
-resource "aws_ecs_task_definition" "renderer" {
-  family                   = "renderer-task"
+resource "aws_ecs_task_definition" "renderer_cloudmap" {
+  family                   = "renderer-cloudmap-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -97,15 +97,15 @@ resource "aws_ecs_task_definition" "renderer" {
 
   container_definitions = jsonencode([
     {
-      name      = "renderer"
+      name      = "renderer-cloudmap"
       image     = "grafana/grafana-image-renderer:3.12.5"
       portMappings = [{ containerPort = 8081 }]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
-          awslogs-group         = "/ecs/renderer"
+          awslogs-group         = "/ecs/renderer-cloudmap"
           awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = "renderer"
+          awslogs-stream-prefix = "renderer-cloudmap"
           awslogs-create-group  = "true"
         }
       }
@@ -113,8 +113,8 @@ resource "aws_ecs_task_definition" "renderer" {
   ])
 }
 
-resource "aws_ecs_task_definition" "redis" {
-  family                   = "redis-task"
+resource "aws_ecs_task_definition" "redis_cloudmap" {
+  family                   = "redis-cloudmap-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
@@ -123,15 +123,15 @@ resource "aws_ecs_task_definition" "redis" {
 
   container_definitions = jsonencode([
     {
-      name      = "redis"
+      name      = "redis-cloudmap"
       image     = "redis:latest"
       portMappings = [{ containerPort = 6379 }]
       logConfiguration = {
         logDriver = "awslogs",
         options = {
-          awslogs-group         = "/ecs/redis"
+          awslogs-group         = "/ecs/redis-cloudmap"
           awslogs-region        = "us-east-1"
-          awslogs-stream-prefix = "redis"
+          awslogs-stream-prefix = "redis-cloudmap"
           awslogs-create-group  = "true"
         }
       }
@@ -142,8 +142,8 @@ resource "aws_ecs_task_definition" "redis" {
 # ---------------------------------------------
 # ECS Services with Cloud Map
 # ---------------------------------------------
-resource "aws_service_discovery_service" "grafana" {
-  name = "grafana"
+resource "aws_service_discovery_service" "grafana_cloudmap" {
+  name = "grafana-cloudmap"
   dns_config {
     namespace_id = var.cloudmap_namespace_id
     dns_records {
@@ -154,8 +154,8 @@ resource "aws_service_discovery_service" "grafana" {
   }
 }
 
-resource "aws_service_discovery_service" "renderer" {
-  name = "renderer"
+resource "aws_service_discovery_service" "renderer_cloudmap" {
+  name = "renderer-cloudmap"
   dns_config {
     namespace_id = var.cloudmap_namespace_id
     dns_records {
@@ -166,8 +166,8 @@ resource "aws_service_discovery_service" "renderer" {
   }
 }
 
-resource "aws_service_discovery_service" "redis" {
-  name = "redis"
+resource "aws_service_discovery_service" "redis_cloudmap" {
+  name = "redis-cloudmap"
   dns_config {
     namespace_id = var.cloudmap_namespace_id
     dns_records {
@@ -178,10 +178,10 @@ resource "aws_service_discovery_service" "redis" {
   }
 }
 
-resource "aws_ecs_service" "grafana" {
-  name            = "rajesh-grafana-svc"
+resource "aws_ecs_service" "grafana_cloudmap" {
+  name            = "rajesh-grafana-cloudmap-svc"
   cluster         = var.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.grafana.arn
+  task_definition = aws_ecs_task_definition.grafana_cloudmap.arn
   launch_type     = "FARGATE"
   desired_count   = var.grafana_desired_count
   enable_execute_command = true
@@ -191,20 +191,20 @@ resource "aws_ecs_service" "grafana" {
     assign_public_ip = false
   }
   service_registries {
-    registry_arn = aws_service_discovery_service.grafana.arn
+    registry_arn = aws_service_discovery_service.grafana_cloudmap.arn
   }
   load_balancer {
-    target_group_arn = aws_lb_target_group.grafana.arn
-    container_name   = "grafana"
+    target_group_arn = aws_lb_target_group.grafana_cloudmap.arn
+    container_name   = "grafana-cloudmap"
     container_port   = 3000
   }
-  depends_on = [aws_lb_listener_rule.grafana]
+  depends_on = [aws_lb_listener_rule.grafana_cloudmap]
 }
 
-resource "aws_ecs_service" "renderer" {
-  name            = "rajesh-renderer-svc"
+resource "aws_ecs_service" "renderer_cloudmap" {
+  name            = "rajesh-renderer-cloudmap-svc"
   cluster         = var.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.renderer.arn
+  task_definition = aws_ecs_task_definition.renderer_cloudmap.arn
   launch_type     = "FARGATE"
   desired_count   = var.renderer_desired_count
   enable_execute_command = true
@@ -214,14 +214,14 @@ resource "aws_ecs_service" "renderer" {
     assign_public_ip = false
   }
   service_registries {
-    registry_arn = aws_service_discovery_service.renderer.arn
+    registry_arn = aws_service_discovery_service.renderer_cloudmap.arn
   }
 }
 
-resource "aws_ecs_service" "redis" {
-  name            = "rajesh-redis-svc"
+resource "aws_ecs_service" "redis_cloudmap" {
+  name            = "rajesh-redis-cloudmap-svc"
   cluster         = var.ecs_cluster_id
-  task_definition = aws_ecs_task_definition.redis.arn
+  task_definition = aws_ecs_task_definition.redis_cloudmap.arn
   launch_type     = "FARGATE"
   desired_count   = var.redis_desired_count
   enable_execute_command = true
@@ -231,27 +231,27 @@ resource "aws_ecs_service" "redis" {
     assign_public_ip = false
   }
   service_registries {
-    registry_arn = aws_service_discovery_service.redis.arn
+    registry_arn = aws_service_discovery_service.redis_cloudmap.arn
   }
 }
 
 # ---------------------------------------------
 # Auto Scaling Policies
 # ---------------------------------------------
-resource "aws_appautoscaling_target" "grafana" {
+resource "aws_appautoscaling_target" "grafana_cloudmap" {
   max_capacity       = var.grafana_autoscaling_max
   min_capacity       = var.grafana_autoscaling_min
-  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.grafana.name}"
+  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.grafana_cloudmap.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "grafana" {
-  name               = "cpu-autoscaling-grafana"
+resource "aws_appautoscaling_policy" "grafana_cloudmap" {
+  name               = "cpu-autoscaling-grafana-cloudmap"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.grafana.resource_id
-  scalable_dimension = aws_appautoscaling_target.grafana.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.grafana.service_namespace
+  resource_id        = aws_appautoscaling_target.grafana_cloudmap.resource_id
+  scalable_dimension = aws_appautoscaling_target.grafana_cloudmap.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.grafana_cloudmap.service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
@@ -263,20 +263,20 @@ resource "aws_appautoscaling_policy" "grafana" {
   }
 }
 
-resource "aws_appautoscaling_target" "renderer" {
+resource "aws_appautoscaling_target" "renderer_cloudmap" {
   max_capacity       = var.renderer_autoscaling_max
   min_capacity       = var.renderer_autoscaling_min
-  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.renderer.name}"
+  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.renderer_cloudmap.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "renderer" {
-  name               = "cpu-autoscaling-renderer"
+resource "aws_appautoscaling_policy" "renderer_cloudmap" {
+  name               = "cpu-autoscaling-renderer-cloudmap"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.renderer.resource_id
-  scalable_dimension = aws_appautoscaling_target.renderer.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.renderer.service_namespace
+  resource_id        = aws_appautoscaling_target.renderer_cloudmap.resource_id
+  scalable_dimension = aws_appautoscaling_target.renderer_cloudmap.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.renderer_cloudmap.service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
@@ -288,20 +288,20 @@ resource "aws_appautoscaling_policy" "renderer" {
   }
 }
 
-resource "aws_appautoscaling_target" "redis" {
+resource "aws_appautoscaling_target" "redis_cloudmap" {
   max_capacity       = var.redis_autoscaling_max
   min_capacity       = var.redis_autoscaling_min
-  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.redis.name}"
+  resource_id        = "service/${replace(var.ecs_cluster_id, "arn:aws:ecs:us-east-1:[^:]+:cluster/", "")}/${aws_ecs_service.redis_cloudmap.name}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "redis" {
-  name               = "cpu-autoscaling-redis"
+resource "aws_appautoscaling_policy" "redis_cloudmap" {
+  name               = "cpu-autoscaling-redis-cloudmap"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.redis.resource_id
-  scalable_dimension = aws_appautoscaling_target.redis.scalable_dimension
-  service_namespace  = aws_appautoscaling_target.redis.service_namespace
+  resource_id        = aws_appautoscaling_target.redis_cloudmap.resource_id
+  scalable_dimension = aws_appautoscaling_target.redis_cloudmap.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.redis_cloudmap.service_namespace
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
