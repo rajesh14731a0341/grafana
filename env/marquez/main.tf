85cbdb8f5538e269a392960345fbc4e6ec0eb704
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-24 18:50:45 +0530

    Initial commit for maruez ECS with ALB

--- env/marquez/main.tf
+++ env/marquez/main.tf
@@ -4,13 +4,15 @@ module "marquez" {
   ecs_cluster_id     = var.ecs_cluster_id
   ecs_cluster_name   = var.ecs_cluster_name
   vpc_id             = var.vpc_id
-  public_subnet_ids  = var.public_subnet_ids
   private_subnet_ids = var.private_subnet_ids
   security_group_id  = var.security_group_id
   execution_role_arn = var.execution_role_arn
   task_role_arn      = var.task_role_arn
   alb_name           = var.alb_name
   nlb_name           = var.nlb_name
+
+  dockerhub_username = var.dockerhub_username
+  dockerhub_password = var.dockerhub_password
 
   marquez_api_desired_count          = var.marquez_api_desired_count
   marquez_api_autoscaling_min        = var.marquez_api_autoscaling_min
