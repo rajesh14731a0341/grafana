7825a86811b0ae51d6bfdce34715e30f3e877a41
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-24 13:06:10 +0530

    Initial commit for maruez ECS with ALB

--- env/marquez/outputs.tf
+++ env/marquez/outputs.tf
@@ -1,7 +1,15 @@
 output "marquez_api_url" {
-  value = module.marquez.api_url
+  value = module.marquez.marquez_api_url
 }
 
 output "marquez_web_url" {
-  value = module.marquez.web_url
+  value = module.marquez.marquez_web_url
+}
+
+output "marquez_api_service_name" {
+  value = module.marquez.marquez_api_service_name
+}
+
+output "marquez_web_service_name" {
+  value = module.marquez.marquez_web_service_name
 }
--- modules/marquez/outputs.tf
+++ modules/marquez/outputs.tf
@@ -1,3 +1,13 @@
+output "marquez_api_url" {
+  description = "Public URL for Marquez API"
+  value       = "http://${data.aws_lb.public_alb.dns_name}/marquez"
+}
+
+output "marquez_web_url" {
+  description = "Public URL for Marquez Web"
+  value       = "http://${data.aws_lb.public_alb.dns_name}/marquez-web"
+}
+
 output "marquez_api_service_name" {
   value = aws_ecs_service.marquez_api.name
 }
