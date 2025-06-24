a64d858d75cd3d3702c343fae0c353e3c6713418
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-24 13:03:19 +0530

    Initial commit for maruez ECS with ALB

--- modules/marquez/variables.tf
+++ modules/marquez/variables.tf
@@ -1,3 +1,4 @@
+# Required existing variables
 variable "ecs_cluster_id" {}
 variable "ecs_cluster_name" {}
 variable "vpc_id" {}
@@ -15,4 +16,46 @@ variable "marquez_api_desired_count" {
 }
 variable "marquez_web_desired_count" {
   type = number
+}
+
+# ✅ Missing but referenced variables in your env/marquez/main.tf
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "nlb_name" {
+  description = "Name of the internal NLB"
+  type        = string
+}
+
+variable "marquez_api_autoscaling_min" {
+  description = "Min number of Marquez API tasks"
+  type        = number
+}
+
+variable "marquez_api_autoscaling_max" {
+  description = "Max number of Marquez API tasks"
+  type        = number
+}
+
+variable "marquez_api_autoscaling_cpu_target" {
+  description = "Target CPU utilization for Marquez API autoscaling"
+  type        = number
+}
+
+variable "marquez_web_autoscaling_min" {
+  description = "Min number of Marquez Web tasks"
+  type        = number
+}
+
+variable "marquez_web_autoscaling_max" {
+  description = "Max number of Marquez Web tasks"
+  type        = number
+}
+
+variable "marquez_web_autoscaling_cpu_target" {
+  description = "Target CPU utilization for Marquez Web autoscaling"
+  type        = number
 }
