46e7ea04cbae9d182e6294a6073770578c4ffe13
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-27 21:29:54 +0530

    Add Marquez ECS setup with ALB and private IP-based communication

--- modules/marquez/main.tf
+++ modules/marquez/main.tf
@@ -159,7 +159,7 @@ resource "aws_ecs_task_definition" "api" {
     }]
 
     environment = [
-      { name = "MARQUEZ_POSTGRES_HOST", value = data.aws_lb.internal_nlb.dns_name },
+      { name = "MARQUEZ_POSTGRES_HOST", value = "10.0.11.152" },
       { name = "MARQUEZ_POSTGRES_PORT", value = "5432" },
       { name = "MARQUEZ_POSTGRES_USER", value = "marquez" },
       { name = "MARQUEZ_POSTGRES_PASSWORD", value = "marquez" },
