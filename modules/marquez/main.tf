6ae20b5eb29f93b89415437669e7ec5ded6f838e
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-27 19:28:16 +0530

    Add Marquez ECS setup with ALB and private IP-based communication

--- modules/marquez/main.tf
+++ modules/marquez/main.tf
@@ -153,7 +153,7 @@ resource "aws_ecs_task_definition" "api" {
 
   container_definitions = jsonencode([{
     name  = "marquez-api"
-    image = "736747734611.dkr.ecr.us-east-1.amazonaws.com/project:marquez-api-prv-ip"
+    image = "736747734611.dkr.ecr.us-east-1.amazonaws.com/project:marquez-api-prv-ip-v2"
     portMappings = [{ containerPort = 5000 }]
 
     environment = [
