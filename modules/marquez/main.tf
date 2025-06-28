008856ab352d742fde4ad96a25a2abcdec4f1db3
Rajesh <puchakayalarajesh341@gmail.com>
2025-06-28 12:57:06 +0530

    Add Marquez ECS setup with ALB and private IP-based communication

--- modules/marquez/main.tf
+++ modules/marquez/main.tf
@@ -139,7 +139,7 @@ resource "aws_cloudwatch_log_group" "db_logs" {
   retention_in_days = 7
 }
 
-#####################
+####################
 # Task Definitions
 ######################
 resource "aws_ecs_task_definition" "api" {
