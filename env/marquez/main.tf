af35a6c818d83c61e51a4462e4110553aa7f1eeb
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-11 18:48:50 +0530

    Initial commit for marquez_vector_stack with Nginx, Vector, and config updates

--- env/marquez/main.tf
+++ env/marquez/main.tf
@@ -18,21 +18,21 @@ module "marquez_stack" {
   marquez_postgres_user              = var.marquez_postgres_user
   marquez_postgres_password          = var.marquez_postgres_password
   marquez_postgres_db                = var.marquez_postgres_db
-
   marquez_api_desired_count          = var.marquez_api_desired_count
   marquez_api_autoscaling_min        = var.marquez_api_autoscaling_min
   marquez_api_autoscaling_max        = var.marquez_api_autoscaling_max
   marquez_api_autoscaling_cpu_target = var.marquez_api_autoscaling_cpu_target
-
   marquez_web_desired_count          = var.marquez_web_desired_count
   marquez_web_autoscaling_min        = var.marquez_web_autoscaling_min
   marquez_web_autoscaling_max        = var.marquez_web_autoscaling_max
   marquez_web_autoscaling_cpu_target = var.marquez_web_autoscaling_cpu_target
-
   # Required for Marquez module due to shared S3 config handling
   vector_config_bucket               = var.vector_config_bucket
   nginx_config_bucket                = var.nginx_config_bucket
   config_s3_bucket_name              = var.config_s3_bucket_name
+  nginx_autoscaling_min             = var.nginx_autoscaling_min
+  nginx_autoscaling_max             = var.nginx_autoscaling_max
+  nginx_autoscaling_cpu_target      = var.nginx_autoscaling_cpu_target
 }
 
 module "vector_stack" {
