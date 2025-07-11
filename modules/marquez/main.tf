83e09242b515d66275d1ef9b545837c7b15efa3c
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-11 16:10:16 +0530

    Initial commit for marquez_vector_stack with Nginx, Vector, and config updates

--- modules/marquez/main.tf
+++ modules/marquez/main.tf
@@ -583,3 +583,177 @@ resource "aws_ecs_service" "clickhouse" {
 
   health_check_grace_period_seconds = 60
 }
+
+#########################
+# Upload nginx.conf to S3
+#########################
+resource "aws_s3_object" "nginx_template" {
+  bucket = var.nginx_config_bucket
+  key    = "nginx.template"
+  source = "${path.root}/../../docker/nginx/nginx.template"
+  etag   = filemd5("${path.root}/../../docker/nginx/nginx.template")
+}
+
+
+#########################
+# CloudWatch Log Group
+#########################
+resource "aws_cloudwatch_log_group" "nginx_logs" {
+  name              = "/ecs/nginx-vector-prv-ip"
+  retention_in_days = 7
+}
+
+#########################
+# Task Definition
+#########################
+resource "aws_ecs_task_definition" "nginx" {
+  family                   = "nginx-vector-prv-ip"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "512"
+  memory                   = "1024"
+  execution_role_arn       = var.execution_role_arn
+  task_role_arn            = var.task_role_arn
+
+  container_definitions = jsonencode([{
+    name        = "nginx"
+    image       = "nginx:alpine"
+    essential   = true
+    portMappings = [
+      { containerPort = 80 }
+    ]
+    command = [
+      "sh",
+      "-c",
+      "apk add --no-cache envsubst && \
+       aws s3 cp s3://${var.nginx_config_bucket}/nginx.template /etc/nginx/nginx.template && \
+       envsubst < /etc/nginx/nginx.template > /etc/nginx/nginx.conf && \
+       nginx -g 'daemon off;'"
+    ]
+    environment = [
+      {
+        name  = "AWS_REGION"
+        value = var.region
+      },
+      {
+        name  = "VECTOR_HOST"
+        value = "vector-prv-ip"
+      },
+      {
+        name  = "VECTOR_PORT"
+        value = "8686"
+      }
+    ]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = aws_cloudwatch_log_group.nginx_logs.name
+        awslogs-region        = var.region
+        awslogs-stream-prefix = "ecs"
+      }
+    }
+  }])
+}
+
+
+#########################
+# Target Group (NGINX)
+#########################
+resource "aws_lb_target_group" "nginx_vector_tg" {
+  name        = "nginx-vector-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = "/healthz"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    matcher             = "200"
+  }
+}
+
+
+#########################
+# Listener Rule for /ol/vec
+#########################
+resource "aws_lb_listener_rule" "nginx_vector_path_rule" {
+  listener_arn = var.alb_listener_arn
+  priority     = 60
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_vector_tg.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/ol-vector*", "/healthz"]
+    }
+  }
+}
+
+
+#########################
+# ECS Service
+#########################
+resource "aws_ecs_service" "nginx" {
+  name                   = "nginx-vector-prv-ip"
+  cluster                = var.ecs_cluster_id
+  task_definition        = aws_ecs_task_definition.nginx.arn
+  desired_count          = var.nginx_desired_count
+  launch_type            = "FARGATE"
+  enable_execute_command = true
+
+  network_configuration {
+    subnets          = var.private_subnet_ids
+    security_groups  = [var.security_group_id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.nginx_vector_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+
+  depends_on = [
+    aws_lb_listener_rule.nginx_vector_path_rule,
+    aws_cloudwatch_log_group.nginx_logs,
+    aws_s3_object.nginx_template
+  ]
+
+  health_check_grace_period_seconds = 60
+}
+
+#########################
+# Auto Scaling (Optional)
+#########################
+resource "aws_appautoscaling_target" "nginx" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/${var.ecs_cluster_name}/nginx-vector-prv-ip"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "nginx_cpu" {
+  name               = "nginx-cpu-autoscaling"
+  service_namespace  = "ecs"
+  resource_id        = aws_appautoscaling_target.nginx.resource_id
+  scalable_dimension = aws_appautoscaling_target.nginx.scalable_dimension
+  policy_type        = "TargetTrackingScaling"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value       = 50.0
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+  }
+}
