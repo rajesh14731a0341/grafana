cca24cf55ee62ea8808b5c491c99e41512891b10
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-13 13:45:04 +0530

    Initial commit for grafana_vector_stack with Nginx, Vector, and config updates

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -80,7 +80,7 @@ data "aws_lb_listener" "public_listener" {
 
 
 resource "aws_lb_listener_rule" "grafana_rule" {
-  listener_arn = aws_lb_listener.public_listener.arn
+  listener_arn = data.aws_lb_listener.public_listener.arn
   priority     = 100
 
   action {
@@ -97,7 +97,7 @@ resource "aws_lb_listener_rule" "grafana_rule" {
 
 
 resource "aws_lb_listener_rule" "renderer_rule" {
-  listener_arn = aws_lb_listener.public_listener.arn
+  listener_arn = data.aws_lb_listener.public_listener.arn
   priority     = 200
 
   action {
