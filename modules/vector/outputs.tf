06b1c991513333a127c4e597b8d6ed90296cd1f5
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-16 16:49:31 +0530

    Initial commit for marquez_vector_stack with Nginx, Vector, and config updates

--- modules/vector/outputs.tf
+++ modules/vector/outputs.tf
@@ -1,3 +1,7 @@
+#######################
+# ECS Service Outputs
+#######################
+
 output "vector_service_name" {
   description = "Name of the ECS service running vector"
   value       = aws_ecs_service.vector.name
@@ -13,12 +17,35 @@ output "nginx_service_name" {
   value       = aws_ecs_service.nginx.name
 }
 
-output "nginx_target_group_arn" {
-  description = "Target group ARN for nginx"
-  value       = aws_lb_target_group.nginx_vector_tg.arn
+#######################
+# Target Group ARNs
+#######################
+
+output "vector_target_group_arn" {
+  description = "Target group ARN for Vector (8686)"
+  value       = aws_lb_target_group.d3po_vector_tg.arn
 }
 
 output "clickhouse_target_group_arn" {
   description = "Target group ARN for ClickHouse (8123)"
-  value       = aws_lb_target_group.clickhouse_tg.arn
+  value       = aws_lb_target_group.d3po_clickhouse_tg.arn
+}
+
+output "nginx_target_group_arn" {
+  description = "Target group ARN for NGINX"
+  value       = aws_lb_target_group.nginx_marquez_tg.arn
+}
+
+#######################
+# Load Balancer DNS (Optional)
+#######################
+
+output "internal_nlb_dns" {
+  description = "Internal NLB DNS name"
+  value       = data.aws_lb.internal_nlb.dns_name
+}
+
+output "public_alb_dns" {
+  description = "Public ALB DNS name"
+  value       = data.aws_lb.public_alb.dns_name
 }
