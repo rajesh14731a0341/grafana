7b6a24ae3fc0663a93fa9b9fc01d14d6b37eaa50
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-16 15:07:45 +0530

    Initial commit for marquez_vector_stack with Nginx, Vector, and config updates

--- env/marquez/backend.tf
+++ env/marquez/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "redshift-data-migration-bucket"
+    bucket = "errorbudget-s3"
     key    = "marquez/test_prv_ip/terraform.tfstate"
     region = "us-east-1"
   }
