079763b9cf811039b6403ae4cbda20c79fcc133b
Rajesh <puchakayalarajesh341@gmail.com>
2025-07-21 19:25:59 +0530

    Add marquez_d3po_promodb_stack_with_targetgroup_wav stack configuration

--- env/marquez/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "marquez_api_url" {
-  value = "http://${module.marquez_stack.alb_dns}/marquez/api"
-}
-
-output "marquez_web_url" {
-  value = "http://${module.marquez_stack.alb_dns}/marquez/web"
-}
